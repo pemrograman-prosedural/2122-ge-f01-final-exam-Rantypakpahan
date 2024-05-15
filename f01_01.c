2fc64a69a464d07d864e19c93cafad042304bd5e
Rantypakpahan <143679143+Rantypakpahan@users.noreply.github.com>
2024-05-15 17:00:36 +0700

    y

--- f01_01.c
+++ f01_01.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "./libs/dorm.h"
+#include "./libs/dorm.h"   
 #include "./libs/student.h"
 
 int main(int _argc, char **_argv) 
